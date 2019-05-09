7a1b30d02e68802c43967e0bca39463cce8a2e77
Kayo Leandro Stabile <kayo.contaon@gmail.com>
2019-05-08 22:42:19 -0300

    segundo commit

--- Ex02.Designer.cs
+++ Ex02.Designer.cs
@@ -30,31 +30,51 @@
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(244, 122);
+            this.comboBox1.Location = new System.Drawing.Point(249, 34);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(206, 21);
             this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(276, 188);
+            this.button1.Location = new System.Drawing.Point(301, 74);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 49);
+            this.button1.Size = new System.Drawing.Size(104, 26);
             this.button1.TabIndex = 1;
             this.button1.Text = "Carregar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(249, 135);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(206, 21);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(203, 176);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(300, 133);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form2";
@@ -67,5 +87,7 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- Ex02.cs
+++ Ex02.cs
@@ -20,9 +20,20 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             FontFamily[] lista = FontFamily.Families;
+            for(int i=0;i<lista.Length;i++) //abre uma lista de fontfamily
+            {
+                comboBox1.Items.Add(lista[i].Name);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FontFamily[] lista = FontFamily.Families;
+            progressBar1.Maximum = lista.Length;
             for(int i=0;i<lista.Length;i++)
             {
                 comboBox1.Items.Add(lista[i].Name);
+                progressBar1.Value = i + 1; //Progress bar começa na posição 1
             }
         }
     }
