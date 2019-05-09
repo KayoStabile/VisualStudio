ace98d177a2cab13a191abcf7e860293f17875c5
Kayo Leandro Stabile <kayo.contaon@gmail.com>
2019-05-08 22:24:28 -0300

    commit inicial

---
+++ Ex01.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show(); //abre o formulario 2 quando clicar no botao
+
+
+            colorDialog1.ShowDialog(); //Abre uma palheta de cor para 
+            BackColor = colorDialog1.Color; // escolher o background do formulario
+       
+        }
+
+        private void novoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.BackColor = Color.Yellow;
+            form2.Show(); //abre o formulario 2 na cor amarela
+
+        }
+    }
+}
